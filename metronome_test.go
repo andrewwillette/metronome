d6959bd7e6f53c62923e0ae9bd69fa03dc67fc5a
andrewwillette <willette.andrew1846@gmail.com>
2022-05-20 22:35:42 -0500

    master: adding some tests

--- metronome_test.go
+++ metronome_test.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_getHeader(t *testing.T) {
-	if GetApplicationTitle() != "Metronome" {
-		t.Error("header incorrect")
+func Test_bpm2bps(t *testing.T) {
+	tts := []struct {
+		exptd time.Duration
+		bpm   int
+	}{
+		{
+			exptd: time.Millisecond * 60,
+			bpm:   1000,
+		},
+		{
+			exptd: time.Millisecond * 667,
+			bpm:   90,
+		},
+		{
+			exptd: time.Second,
+			bpm:   60,
+		},
+	}
+	for _, v := range tts {
+		result := bpm2bps(v.bpm)
+		assert.Equal(t, v.exptd, result.Round(time.Millisecond))
 	}
 }
 
-// func Test_getWindow(t *testing.T) {
-// // window.FullScreen
-// assert true
-// }
+func Test_getFrames(t *testing.T) {
+	tts := []struct {
+		provided, expected []string
+	}{
+		{
+			provided: []string{"G", "B"},
+			expected: []string{"G", " B"},
+		},
+	}
+	for _, v := range tts {
+		frames := getFrames(v.provided)
+		assert.Equal(t, frames, v.expected)
+	}
+}
