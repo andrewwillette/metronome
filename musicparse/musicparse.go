b5845890cb8ddaeca108a6d1cb4b34b02824e4e6
andrewwillette <willette.andrew1846@gmail.com>
2022-06-25 16:40:45 -0500

    eh: da

---
+++ musicparse/musicparse.go
@@ -0,0 +1,50 @@
+package musicparse
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Song struct {
+	Title    string `yaml:"song,omitempty"`
+	Sections struct {
+		ASection [][]string `yaml:"a,flow,omitempty"`
+		BSection [][]string `yaml:"b,flow,omitempty"`
+		CSection [][]string `yaml:"c,flow,omitempty"`
+	}
+}
+
+func readSong(fpath string) Song {
+	yfile, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		panic(err)
+	}
+	var song Song
+	err = yaml.Unmarshal(yfile, &song)
+	if err != nil {
+		panic(err)
+	}
+	return song
+}
+
+func readSongsFromDir(fpath string) []Song {
+	f, err := os.Open(fpath)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	files, err := f.Readdir(0)
+	if err != nil {
+		panic(err)
+	}
+	songs := []Song{}
+	for _, v := range files {
+		song := readSong(filepath.Join(fpath, v.Name()))
+		songs = append(songs, song)
+	}
+	return songs
+}
